da0ee9fa25e18056c6fa4c15a350b861acd3e461
Hui._. <131990545+h2st0ry@users.noreply.github.com>
2024-11-19 23:07:04 +0900

    Update Header.jsx

--- reactweb/src/component/Header.jsx
+++ reactweb/src/component/Header.jsx
@@ -2,18 +2,17 @@ import React from "react";
 import MyCategory from "./MyCategory";
 import CategoryBar from "./CategoryBar";
 
-
 export default function Header() {
     return (
-        <>
+        <div className="fixed">
             <div className="header">
                 <div className="SiteLogo">
-                    <div className="SiteName" href="/">We_eb</div>
+                    <a className="SiteName" href="/">We_eb</a>
                     <img src="/gromit.png" alt="Gromit" />
                 </div>
             </div>
             <MyCategory />
             <CategoryBar />
-        </>
+        </div>
     );
 }
